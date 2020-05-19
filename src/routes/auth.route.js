5206fddc782607f6404ff63a5623ebfb4e5f2b0f
gabteezy14@gmail.com <gabteezy14@gmail.com>
2020-05-19 05:39:54 +0100

    feat: add authorization helper

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,13 +1,18 @@
 import express from 'express';
-import { createUser, logInUser, getLoginUrlFromGoogle } from '../controller/auth.controller';
+import {
+  createUser, logInUser, getLoginUrlFromGoogle, getASpecificUser
+} from '../controller/auth.controller';
 import { validateInput } from '../middleware/schemaValidation';
 import { authenticationSchema, loginSchema } from '../middleware/authentication';
 import { emailPhoneValidator } from '../middleware/validation/index';
+import Security from '../utils/security';
 
 const authRoute = express.Router();
 const BASE_URL = '/auth';
 authRoute.post(`${BASE_URL}/createUser`, validateInput(authenticationSchema), emailPhoneValidator, createUser);
 authRoute.post(`${BASE_URL}/signIn`, validateInput(loginSchema), logInUser);
+authRoute.get(`${BASE_URL}/user`, Security.verifyTokenMiddleWare, getASpecificUser);
+
 authRoute.get(`${BASE_URL}/getUrl`, getLoginUrlFromGoogle);
 
 export default authRoute;
