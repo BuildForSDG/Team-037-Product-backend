6cef686dd6b05b889240b900ed76658159ed4380
gabteezy14@gmail.com <gabteezy14@gmail.com>
2020-05-20 08:02:29 +0100

    chore: add swagger docs  for documentation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express, { json, urlencoded } from 'express';
 import 'dotenv/config';
 import logger from 'morgan';
 import debug from 'debug';
+import swaggerJsDocs from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 import routes from './routes';
 
 const log = debug('dev');
@@ -10,10 +12,41 @@ const app = express();
 app.use(logger('dev'));
 app.use(json());
 app.use(urlencoded({ extended: false }));
-app.use('/api/v1', routes);
 
 const PORT = process.env.PORT || 8000;
+const server = `http://localhost:${PORT}`;
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: 'PROGRAMMERS_HUB_FORUM',
+      description:
+        'This application is developed towards creating an interactive platform for Programmers Hub Members',
+      contact: {
+        name: 'Fortune'
+      },
+      servers: `${server}`
+    },
+    basePath: '/api/v1'
+  },
+  // loop on all route
+  apis: ['**/**.route.js'] // pass all in array
+};
+
+const swaggerDOCS = swaggerJsDocs(swaggerOptions);
+
+app.use('/api/v1', routes);
+
+app.use('/api-docs', routes);
+
+app.use('', swaggerUi.serve, swaggerUi.setup(swaggerDOCS));
+
+app.get('/api/v1', (req, res) => {
+  res.status(200).json({ message: 'Welcome to programmers_Hub_Forumn API' });
+});
+
+
 app.set('port', PORT);
-app.listen(PORT, () => log(`App listening on port ${PORT}!`));
+app.listen(PORT, () => log(`App listening on port ${server}`));
 
 export default app;
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-tabs */
 import express from 'express';
 import { createUser, logInUser, getLoginUrlFromGoogle } from '../controller/auth.controller';
 import { validateInput } from '../middleware/schemaValidation';
@@ -6,8 +7,103 @@ import { emailPhoneValidator } from '../middleware/validation/index';
 
 const authRoute = express.Router();
 const BASE_URL = '/auth';
+
+/**
+ * @swagger
+ *
+ * /auth/creatUser:
+ *   post:
+ *     tags:
+ *       - User Register
+ *     description: User can create account.
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: firstName
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: lastName
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: email
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: phone
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: password
+ *         description: User's password.
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: imageUrl
+ *         description: user profile image
+ *         in: formData
+ *         required: false
+ *         type: string
+ *       - name: state
+ *         description: State where the users is from.
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: confirmationType
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: address
+ *         in: formData
+ *         required: true
+ *         type: string
+ *     responses:
+ *       201:
+ *         description: Account created successfully
+ *       409:
+ *         description: User already exists
+ *       422:
+ *         description: Validation Error
+ *       500:
+ *         description: Server error
+ */
 authRoute.post(`${BASE_URL}/createUser`, validateInput(authenticationSchema), emailPhoneValidator, createUser);
+
+
+/**
+ * @swagger
+ *
+ * /auth/login:
+ *   post:
+ *     tags:
+ *       - All Users Login
+ *     description: Login to account, for customer, vendor, and pro
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: username
+ *         description: Email .
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: password
+ *         description: User's password.
+ *         in: formData
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Logged in successfully
+ *       404:
+ *         description: User does not exists
+ *       422:
+ *         description: Validation Error
+ *       500:
+ *         description: Server error
+ */
 authRoute.post(`${BASE_URL}/signIn`, validateInput(loginSchema), logInUser);
-authRoute.get(`${BASE_URL}/getUrl`, getLoginUrlFromGoogle);
+
+// authRoute.get(`${BASE_URL}/getUrl`, getLoginUrlFromGoogle);
 
 export default authRoute;
